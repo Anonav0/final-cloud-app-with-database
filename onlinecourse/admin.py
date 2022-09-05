26892aeca5eef92ea701b638c49d3727324c0de6
Anonav0 <swarnavokhanra@gmail.com>
2022-09-05 17:46:23 +0000

    exam result template url and minor updates

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,14 +4,22 @@ from .models import Course, Lesson, Instructor, Learner
 from .models import Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-class ChoiceInLine(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
-class QuestionAdmin(admin.ModelAdmin):
-    model = Question
-    inlines = [ChoiceInLine]
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'correct')
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    inlines = [ChoiceInline]
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text',)
+    search_fields = ['question_text',]
 
 
 class LessonInline(admin.StackedInline):
@@ -21,7 +29,7 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -34,6 +42,7 @@ class LessonAdmin(admin.ModelAdmin):
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,12 +102,9 @@ class Enrollment(models.Model):
     # Has question content
     # Other fields and methods you would like to design
 class Question(models.Model):
-    # Foreign key to Course
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    # question text
-    question_text = models.TextField()
-    # question grade/mark
-    grade = models.IntegerField(default=0)
+    question_text = models.CharField(null=False, max_length=500)
+    grade = models.IntegerField()
 
 
     # <HINT> A sample model method to calculate if learner get the score of the question
@@ -127,11 +124,9 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-
-    question = models.ForeignKey(Question, on_delete= models.CASCADE)
-    choice_text = models.TextField()
-    is_correct = models.BooleanField(default=False)
-
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(null=False, max_length=500)
+    is_correct = models.BooleanField()
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
